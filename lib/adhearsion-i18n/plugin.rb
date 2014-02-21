e27550b80ff9ca96c887223409a8145b4659e165
Ben Klang <bklang@mojolingo.com>
2014-02-21 18:57:07 -0500

    Configure I18n load path

--- lib/adhearsion-i18n/plugin.rb
+++ lib/adhearsion-i18n/plugin.rb
@@ -3,6 +3,12 @@
 class AdhearsionI18n::Plugin < Adhearsion::Plugin
   init :i18n do
     ::Adhearsion::CallController.mixin ::Adhearsion::I18n::CallControllerMethods
+
+    config.locale_path.each do |dir|
+      logger.debug "Adding #{dir} to the I18n load path"
+      I18n.load_path << "#{dir}/**/*.yml"
+    end
+
     logger.info "Adhearsion I18n loaded"
   end
 
