6886a01825a0ba8661b87aad6e6d636b60aa44f4
Ben Klang <bklang@mojolingo.com>
2014-02-21 19:05:11 -0500

    Typo

--- lib/adhearsion-i18n/plugin.rb
+++ lib/adhearsion-i18n/plugin.rb
@@ -2,7 +2,7 @@
 
 class AdhearsionI18n::Plugin < Adhearsion::Plugin
   init :i18n do
-    ::Adhearsion::CallController.mixin ::Adhearsion::I18n::CallControllerMethods
+    ::Adhearsion::CallController.mixin ::AdhearsionI18n::CallControllerMethods
 
     config.locale_path.each do |dir|
       logger.debug "Adding #{dir} to the I18n load path"
